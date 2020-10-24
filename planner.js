cef8f40a3180cbed338f058ac7069331aa975c61
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-24 01:24:43 +0100

    added psuedo code comments

---
+++ planner.js
@@ -0,0 +1,17 @@
+// reference variables for DOM
+
+// api call
+
+// init function
+
+// set items to storage
+
+// get items from storage
+
+// render page - current and 5 day forecast
+
+// render search history function
+
+// event listener for user search input
+
+// event listener on search history clicks using event delagation on #seachHistoryList
